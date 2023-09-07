ea774789470baa9a364bb565bd441ed03b5600c6
kamaleshfssa <118255046+kamaleshfssa@users.noreply.github.com>
2023-09-07 16:12:42 +0530

    Docs Implemented

--- src/main/java/com/fssa/needstobedone/services/UserService.java
+++ src/main/java/com/fssa/needstobedone/services/UserService.java
@@ -5,51 +5,56 @@ import com.fssa.needstobedone.exception.DAOException;
 import com.fssa.needstobedone.exception.ServiceException;
 import com.fssa.needstobedone.exception.ValidationException;
 import com.fssa.needstobedone.model.User;
+import com.fssa.needstobedone.utils.PasswordUtil;
 import com.fssa.needstobedone.validation.UserValidator;
 
 /**
- * The UserService class provides user-related operations such as registration and login.
+ * The UserService class provides user-related operations such as registration
+ * and login.
  */
 public class UserService {
 
-    /**
-     * Registers a new user.
-     *
-     * @param user The user to be registered.
-     * @return true if the user is registered successfully, false otherwise.
-     * @throws ServiceException If there is an issue registering the user.
-     */
-    public boolean registerUser(User user) throws ServiceException {
-        UserDAO userDAO = new UserDAO();
-        try {
-            UserValidator.validateUser(user);
-            userDAO.createUser(user);
-            return true;
-        } catch (DAOException | ValidationException e) {
-            throw new ServiceException(e.getMessage());
-        }
-    }
+	/**
+	 * Registers a new user.
+	 *
+	 * @param user The user to be registered.
+	 * @return true if the user is registered successfully, false otherwise.
+	 * @throws ServiceException If there is an issue registering the user.
+	 */
+	public boolean registerUser(User user) throws ServiceException {
+		UserDAO userDAO = new UserDAO();
+		try {
+			UserValidator.validateUser(user);
+			String newPassword = PasswordUtil.hashPassword(user.getPassword());
+			user.setPassword(newPassword);
+			System.out.println(user.getPassword());
+			userDAO.createUser(user);
+			return true;
+		} catch (DAOException | ValidationException e) {
+			throw new ServiceException(e.getMessage());
+		}
+	}
 
-    /**
-     * Logs in a user.
-     *
-     * @param user The user to be logged in.
-     * @return The logged-in user if login is successful.
-     * @throws ServiceException If there is an issue with the login process.
-     */
-    public User logInUser(User user) throws ServiceException {
-        UserDAO userDAO = new UserDAO();
-        try {
-            User gettedUser = userDAO.getUserByEmail(user.getEmail());
-            if (gettedUser == null) {
-                throw new DAOException("User is not registered");
-            }
-            if (!user.getPassword().equals(gettedUser.getPassword())) {
-                throw new ServiceException("Login Failed - password mismatch");
-            }
-        } catch (DAOException e) {
-            throw new ServiceException(e.getMessage());
-        }
-        return user;
-    }
+	/**
+	 * Logs in a user.
+	 *
+	 * @param user The user to be logged in.
+	 * @return The logged-in user if login is successful.
+	 * @throws ServiceException If there is an issue with the login process.
+	 */
+	public User logInUser(User user) throws ServiceException {
+		UserDAO userDAO = new UserDAO();
+		try {
+			User gettedUser = userDAO.getUserByEmail(user.getEmail());
+			if (gettedUser == null) {
+				throw new DAOException("User is not registered");
+			}
+			if (!PasswordUtil.checkPassword(user.getPassword(), gettedUser.getPassword())) {
+				throw new ServiceException("Login Failed - password mismatch");
+			}
+		} catch (DAOException e) {
+			throw new ServiceException(e.getMessage());
+		}
+		return user;
+	}
 }
